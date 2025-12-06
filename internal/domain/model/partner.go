59e42cfdeb2ed9ca987ad776bb533502430527c5
hasseeee <hasegawa.takuya1204@gmail.com>
2025-12-07 05:15:37 +0900

    feat:　パトナーモデルの拡張

--- internal/domain/model/partner.go
+++ internal/domain/model/partner.go
@@ -6,6 +6,9 @@ type Partner struct {
 	ID           uuid.UUID `json:"id"`
 	UserID       uuid.UUID `json:"user_id"`
 	Name         string    `json:"name"`
+	Personality  string    `json:"personality"`
+	HairColor    string    `json:"hair_color"`
+	VoiceType    string    `json:"voice_type"`
 	CurrentStage string    `json:"current_stage"`
 	Stamina      int       `json:"stamina"`
 	Intelligence int       `json:"intelligence"`
