b9450c430fde54b920f7787e94db3c5744e42d34
hasseeee <hasegawa.takuya1204@gmail.com>
2025-12-06 17:36:27 +0900

    feat: userモデルを定義

---
+++ internal/domain/model/user.go
@@ -0,0 +1,11 @@
+package model
+
+import "github.com/google/uuid"
+
+// バッチ処理用にユーザーとパートナー情報をまとめて扱う構造体
+type UserWithPartner struct {
+	UserID       uuid.UUID
+	PartnerID    uuid.UUID
+	PartnerName  string
+	CurrentStage string
+}
